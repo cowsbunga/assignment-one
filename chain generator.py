245e0083ac6134b863ade50a24a0bcf3c436bd3c
cowsbunga <86610385+cowsbunga@users.noreply.github.com>
2024-01-17 13:12:52 +0000

    Update chain generator.py

--- chain generator.py
+++ chain generator.py
@@ -1,3 +1,6 @@
+# given 2 numbers n1 and n2, calculates the number bracelet they give
+#stops when the chain starts reapeating, or when the 102nd element of the chain is reached, to prevent breaking if the chain is infinite
+
 import numpy as np
 import matplotlib.pyplot as plt
 import math
@@ -6,7 +9,11 @@ first = int(input("Enter first number: "))
 second = int(input("Enter second number: "))
 n1 = first
 n2 =  second
+print(n1)
+print(n2)
 i = 0
+
+
 
 while i<100:
     n_next = (n1 + n2) % 10
